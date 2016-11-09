ff2100eeeeee097f282c9b9f6d177623ccfb9cd9
Özgür Akgün <ozgurakgun@gmail.com>
2016-11-09 23:16:55 +0000

    So the value of GH_TOKEN is not printed in travis -- will delete the old token since it was displayed there

--- __internal/deploy.sh
+++ __internal/deploy.sh
@@ -10,7 +10,7 @@ if [ $TRAVIS_PULL_REQUEST != "false" ]; then
     echo "This is pull request number $TRAVIS_PULL_REQUEST."
     echo "The build will be deployed to to the 'previews' repository."
     rev=$(git rev-parse --short HEAD)
-    git clone "https://$GH_TOKEN@github.com/dlrep/previews.git" dlrep-previews
+    git clone "https://github.com/dlrep/previews.git" dlrep-previews
     cd dlrep-previews
     git checkout -B gh-pages
     rm -rf $TRAVIS_PULL_REQUEST
@@ -19,13 +19,14 @@ if [ $TRAVIS_PULL_REQUEST != "false" ]; then
     git config user.email "ozgurakgun@gmail.com"
     git add --all
     git commit -m "Preview build for PR dlrep/dlrep#$TRAVIS_PULL_REQUEST, commit dlrep/dlrep@$rev"
-    git push "https://$GH_TOKEN@github.com/dlrep/previews.git" gh-pages
+    git remote add upstream "https://$GH_TOKEN@github.com/dlrep/production.git"
+    git push upstream gh-pages
 
 elif [ $TRAVIS_BRANCH == "master" ]; then
     echo "This is the master branch."
     echo "The build will be deployed to to the 'production' repository."
     rev=$(git rev-parse --short HEAD)
-    git clone "https://$GH_TOKEN@github.com/dlrep/production.git" dlrep-production
+    git clone "https://github.com/dlrep/production.git" dlrep-production
     cd dlrep-production
     git checkout -B gh-pages
     rm -rf *
@@ -34,7 +35,8 @@ elif [ $TRAVIS_BRANCH == "master" ]; then
     git config user.email "ozgurakgun@gmail.com"
     git add --all
     git commit -m "Deploying commit dlrep/dlrep@$rev"
-    git push "https://$GH_TOKEN@github.com/dlrep/production.git" gh-pages
+    git remote add upstream "https://$GH_TOKEN@github.com/dlrep/production.git"
+    git push upstream gh-pages
 
 else
     echo "Skipping deploy."
