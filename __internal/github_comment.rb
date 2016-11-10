605ae72b3a57eea0603be67400a966e34a480886
Özgür Akgün <ozgurakgun@gmail.com>
2016-11-10 09:52:46 +0000

    Adding some progress info

--- __internal/github_comment.rb
+++ __internal/github_comment.rb
@@ -5,5 +5,15 @@ token   = ENV["CSPLIB_ROBOT_TOKEN"]
 prnum   = ENV["TRAVIS_PULL_REQUEST"]
 comment = ENV["GITHUB_COMMENT"]
 
+puts "Logging in"
+
 client = Octokit::Client.new(:access_token => token)
+
+puts "Logged in"
+
+user = client.user
+puts user.login
+
 client.add_comment("dlrep/dlrep", prnum, comment)
+
+puts "Posted comment"
